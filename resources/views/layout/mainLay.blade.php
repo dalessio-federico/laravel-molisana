c08509e033d7c25911de5661af2b12a50ad5f74f
federico d'alessio <federico1993.d.alessio.1993@gmail.com>
2021-05-06 17:09:40 +0200

    Fine Header

--- resources/views/layout/mainLay.blade.php
+++ resources/views/layout/mainLay.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Coda:wght@400;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset("css\generalstyle.css")}}">
     <title>@yield('titolo') | La Molisana</title>
 </head>
 <body>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,11 +1,20 @@
-<ul>
-    <li>
-        <a href="{{route("Homepage")}}">Home</a>
-    </li>
-    <li>
-        <a href="{{route("Prodotti-Pasta")}}">Prodotti</a>
-    </li>
-    <li>
-        <a href="{{route("News")}}">News</a>
-    </li>
-</ul>
+<header>
+    <div class="container flexColumnCenter">
+        <div class="headerLogo">
+            <img src="{{asset("img\logo.png")}}" alt="La Molisana Logo">
+        </div>
+        <div class="headerNavbar">
+            <ul class="inlineList flexRowSpaceAround">
+                <li>
+                    <a href="{{route("Homepage")}}">Home</a>
+                </li>
+                <li>
+                    <a href="{{route("Prodotti-Pasta")}}">Prodotti</a>
+                </li>
+                <li>
+                    <a href="{{route("News")}}">News</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
